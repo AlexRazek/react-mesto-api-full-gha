8c3e4f6f744df153b4c07e12cdd95156656acac8
Alexander Volotchin <alexrazek@yandex.ru>
2023-07-10 14:22:03 +0300

    sprint_15 add CORS restrictions

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const NotFoundError = require('./utils/errors/not-found-error');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:4000',
   credentials: true,
 }));
 
@@ -30,6 +30,41 @@ const apiLimiter = rateLimit({
 const { PORT = 4000, MONGO_URI = 'mongodb://localhost:27017' } = process.env;
 
 app.use(cookieParser());
+
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'http://localhost:4000',
+  'localhost:4000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  return next();
+});
 
 const routerUser = require('./routes/users');
 const routerCard = require('./routes/cards');
